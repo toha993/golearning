5637bf6275618aa2ba433484f0213d73ee12b592
Mutasim Billah <mutasim18@student.sust.edu>
2021-10-10 12:17:34 +0600

    Delete database/Contracts directory

--- database/Contracts/IBulkRepositoryContext.go
+++
@@ -1,9 +0,0 @@
-package contracts
-
-type IBulkRepositoryContext interface{
-	GetAll() (interface{}, error)
-	GetId(string) (interface{}, error)
-	DeleteById(string) error
-	Insert(interface{}) error
-	Save(interface{},string) error
-}
